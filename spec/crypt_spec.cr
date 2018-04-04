36fefb692f750ddbbc522ed9581507cf56266664
Jens Ludvik Nedregård <jens0512@osloskolen.no>
2018-04-04 12:18:19 +0200

    Test CI (Should fail)

--- spec/crypt_spec.cr
+++ spec/crypt_spec.cr
@@ -1,3 +1,15 @@
 require "./crypt/*"
 
-describe Crypt do; end
+describe Crypt do
+  it "fails 1" do
+    self.should be false
+  end
+
+  it "fails 2" do
+    self.should be false
+  end
+
+  it "fails 3" do
+    self.should be false
+  end
+end
